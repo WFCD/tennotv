d1e1337e05cd700e6d94937d21616a328205de92
Tobiah <tobiah@warframe.gg>
2018-06-05 07:41:37 -0500

    fix: switch to " from ', fix formatting, remove padding spaces

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,8 +68,10 @@ function makeTags(tagArray) {
 function makeRow(video) {
   /* eslint-disable no-plusplus */
   let authorRow = `<td class="author col-md-2"><a href="https://www.youtube.com/channel/${video.youtube_key}" name="${video.account_name}'s Channel" target="_blank" rel="noopener">`;
-  authorRow += video.author_yt_thumbnail ? `<img class='author-img' alt='${video.account_name} src='${video.author_yt_thumbnail }'>` : '';
+  authorRow += video.author_yt_thumbnail ? `<img class="author-img" alt="${video.account_name}" src="${video.author_yt_thumbnail}">` : '';
   authorRow += `<span class='author-name'>${video.account_name}</span></a></td>`;
+  
+  
   return `<tr id="${video.video_id}" class="video-row">
     <td scope="row" class="numRow col-md-1" style>${++lastInd}</th>
     <td class="title col-md-6"><a href="${video.video_id}" target="_blank" rel="noopener" name="${video.video_title}">${video.video_title}</a></td>
