f2cc7acb64281b91ac72a1aa27cf176da9561273
Tobiah <tobiah@protonmail.com>
2018-08-13 09:43:32 -0500

    call setup async calls inside an async setup call
    this should allow it to just finish when it's done... I don't know if i can necessarily force the test router to wait, i'll have to look into it

--- routes/index.js
+++ routes/index.js
@@ -25,32 +25,36 @@ logger.level = process.env.LOG_LEVEL || 'error';
 
 const url = `${serviceAPI}?method=get-content-creators`;
 
-logger.log('debug', `Fetching creators: ${url}`);
-snek.get(url, {headers: {'content-type': 'application/json'}})
-  .then(async fetched => {
+const setup = async () => {
+  try {
+    logger.log('debug', `Fetching creators: ${url}`);
+    const fetched = await snek.get(url, {headers: {'content-type': 'application/json'}});
     deps.creators = fetched.body.map(creator => ({
       name: creator.account_name.replace(/\s/g, '').toLowerCase(),
       id: creator.author_id,
       nameDisp: creator.account_name,
       thumb: creator.youtube_thumbnail,
     }));
-
-    [
-      './routeSetups/root', './routeSetups/creators', './routeSetups/videos',
-      './routeSetups/agreement', './routeSetups/feedback', './routeSetups/404',
-    ].forEach(async setup => {
-      // eslint-disable-next-line global-require, import/no-dynamic-require
-      await require(setup)(deps);
-    });
-
-    // eslint-disable-next-line no-shadow
-    setTimeout(deps => {
-      // eslint-disable-next-line global-require
-      require('./routeSetups/catchAll')(deps);
-    }, 60000, deps);
-  })
-  .catch(error => {
+  } catch (error) {
     logger.log('error', error.stack);
+    deps.creators = [];
+  }
+
+  [
+    './routeSetups/root', './routeSetups/creators', './routeSetups/videos',
+    './routeSetups/agreement', './routeSetups/feedback', './routeSetups/404',
+  ].forEach(async setup => {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    await require(setup)(deps);
   });
+
+  // eslint-disable-next-line no-shadow
+  setTimeout(deps => {
+    // eslint-disable-next-line global-require
+    require('./routeSetups/catchAll')(deps);
+  }, 60000, deps);
+};
+
+setup();
 
 module.exports = router;
