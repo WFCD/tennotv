d345edd62ebe95110d17db9f00f2a8e6ab021682
Tobiah <matej@voboril.org>
2018-08-14 14:40:25 -0500

    linting

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,22 +1,22 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('v1').then(function(cache) {
-            return cache.addAll([
-                '/',
-                'js/utility.js',
-                'js/dataHelper.js',
-                'js/playerControl.js',
-                'js/uiControl.js'
-            ]);
-        })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches
+    .open('v1')
+    .then((cache) => {
+      return cache.addAll([
+        '/',
+        'js/utility.js',
+        'js/dataHelper.js',
+        'js/playerControl.js',
+        'js/uiControl.js',
+      ]);
+    })
+  );
 });
- self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(
-            function (response) {
-                return response || fetch(e.request);
-            }
-        )
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches
+    .match(e.request)
+    .then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
