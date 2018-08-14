33d6d0195f6d007ceeb2cf6a2fd6e7ef163881b9
Tobiah <matej@voboril.org>
2018-08-14 20:22:27 +0000

    more testing with manifest

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,18 +1,16 @@
-/* eslint-disable no-restricted-globals */
 window.self.addEventListener('install', event => {
   event.waitUntil(caches
     .open('v1')
     .then(cache => cache.addAll([
-        '/',
-        'js/utility.js',
-        'js/dataHelper.js',
-        'js/playerControl.js',
-        'js/uiControl.js',
-      ])));
+      '/',
+      'js/utility.js',
+      'js/dataHelper.js',
+      'js/playerControl.js',
+      'js/uiControl.js',
+    ])));
 });
 window.self.addEventListener('fetch', e => {
   e.respondWith(caches
     .match(e.request)
     .then(response => response || fetch(e.request)));
 });
-/* eslint-enable no-restricted-globals */
