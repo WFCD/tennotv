5e306c53eb9dd62e885cc41038522151d2fb1f92
Tobiah <matej@voboril.org>
2018-08-14 21:18:06 +0000

    use just self

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 /* globals caches, fetch */
-window.self.addEventListener('install', event => {
+/* eslint-disable no-restricted-globals */
+self.addEventListener('install', event => {
   event.waitUntil(caches
     .open('v1')
     .then(cache => cache.addAll([
@@ -17,8 +18,9 @@ window.self.addEventListener('install', event => {
       'css/styles.css',
     ])));
 });
-window.self.addEventListener('fetch', e => {
+self.addEventListener('fetch', e => {
   e.respondWith(caches
     .match(e.request)
     .then(response => response || fetch(e.request)));
 });
+/* eslint-enable no-restricted-globals */
