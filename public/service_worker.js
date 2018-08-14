96873d4877fbc64c17493cee258c7b135d85db00
Tobiah <matej@voboril.org>
2018-08-14 20:04:22 +0000

    style cleanups

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,22 +1,19 @@
-self.addEventListener('install', (event) => {
+/* eslint-disable no-restricted-globals */
+window.self.addEventListener('install', event => {
   event.waitUntil(caches
     .open('v1')
-    .then((cache) => {
-      return cache.addAll([
+    .then(cache => cache.addAll([
         '/',
         'js/utility.js',
         'js/dataHelper.js',
         'js/playerControl.js',
         'js/uiControl.js',
-      ]);
-    })
+      ])),
   );
 });
-self.addEventListener('fetch', (e) => {
+window.self.addEventListener('fetch', e => {
   e.respondWith(caches
     .match(e.request)
-    .then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    .then(response => response || fetch(e.request)));
 });
+/* eslint-enable no-restricted-globals */
