ea6baaeee50a9516c7ccdec565e070545e3391e8
Tobiah <tobiah@warframe.gg>
2019-01-07 00:37:07 -0600

    remove cache-busting attempt from service worker

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-restricted-globals, no-console */
 const files = [
   '/',
+  'js/dashControl.js',
   'js/utility.js',
   'js/dataHelper.js',
   'js/playerControl.js',
@@ -16,38 +17,17 @@ const files = [
   'css/styles.css',
   'js/flakeid.min.js',
 ];
-const register = sums => {
-  const filesToRegister = sums ? files.map((file, index) => {
-    if (index > 0) {
-      return `${file}?v=${sums[file]}`;
-    }
-    return file;
-  }) : files;
 
-  self.addEventListener('install', event => {
-    event.waitUntil(caches
-      .open('v1')
-      .then(cache => cache.addAll(filesToRegister)));
-  });
+self.addEventListener('install', event => {
+  event.waitUntil(caches
+    .open('v1')
+    .then(cache => cache.addAll(files)));
+});
 
-  self.addEventListener('fetch', e => {
-    e.respondWith(caches
-      .match(e.request)
-      .then(response => response || fetch(e.request)));
-  });
-};
-
-fetch('/sums.json')
-  .then(response => {
-    if (!response.ok) {
-      console.error('[Tenno.tv][Worker] Something went wrong getting sums in service worker.');
-      return undefined;
-    }
-    return response.json();
-  })
-  .then(register)
-  .catch(error => {
-    console.log('Looks like there was a problem: \n', error);
-  });
+self.addEventListener('fetch', e => {
+  e.respondWith(caches
+    .match(e.request)
+    .then(response => response || fetch(e.request)));
+});
 
 /* eslint-enable no-restricted-globals, no-console */
